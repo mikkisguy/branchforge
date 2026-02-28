bf4a56483ccbf0baa147a6bef03d82fe8cfe0f40
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2026-03-01 00:23:27 +0200

    feat: simplify routing by removing unused HomePage components and consolidating to HomePageStorybookIDE

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import HomePageVN from "./pages/HomePage.vn";
-import HomePageIDE from "./pages/HomePage.ide";
-import HomePageAsymmetric from "./pages/HomePage.asymmetric";
 import HomePageStorybookIDE from "./pages/HomePage.storybook-ide";
-import HomePageAll from "./pages/HomePage.all";
 import { BASE_URL } from "./lib/constants";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
@@ -13,12 +8,7 @@ function App() {
     <ThemeProvider>
       <div className="app">
         <Routes>
-          <Route path={BASE_URL} element={<HomePageAll />} />
-          <Route path={`${BASE_URL}original`} element={<HomePage />} />
-          <Route path={`${BASE_URL}vn`} element={<HomePageVN />} />
-          <Route path={`${BASE_URL}ide`} element={<HomePageIDE />} />
-          <Route path={`${BASE_URL}asymmetric`} element={<HomePageAsymmetric />} />
-          <Route path={`${BASE_URL}storybook-ide`} element={<HomePageStorybookIDE />} />
+          <Route path={BASE_URL} element={<HomePageStorybookIDE />} />
         </Routes>
       </div>
     </ThemeProvider>
